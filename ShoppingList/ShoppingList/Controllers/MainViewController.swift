f0e8436ca3594bae677f373c9a1d080984d9a7d4
Youngjin <bryjna07@nate.com>
2025-07-25 15:21:14 +0900

    Feat: 프로젝트 생성, 세팅

---
+++ ShoppingList/ShoppingList/Controllers/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  ShoppingList
+//
+//  Created by YoungJin on 7/25/25.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+        print("test")
+    }
+
+
+}
+
---
+++ ShoppingList/ShoppingList/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  ShoppingList
+//
+//  Created by YoungJin on 7/25/25.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+       
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        
+        window?.rootViewController = MainViewController()
+        window?.makeKeyAndVisible()
+    }
+}
+
